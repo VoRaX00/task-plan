36ea607b70646e29c2ac70574ac842ddf917c814
nikita <nikitakerzhik@gmail.com>
2024-09-25 12:40:04 +1000

    added group for handler and implement base methods

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -18,5 +18,17 @@ func NewHandler(services *application.Service) *Handler {
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sing-up", h.SignUp)
+		auth.POST("sign-in", h.SignIn)
+	}
+
+	api := router.Group("/api")
+	{
+		api.GET("/tasks", h.GetTasks)
+		api.POST("/create-task", h.CreateTask)
+	}
+
 	return router
 }
---
+++ internal/handler/task.go
@@ -0,0 +1,11 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) GetTasks(c *gin.Context) {
+
+}
+
+func (h *Handler) CreateTask(c *gin.Context) {
+
+}
