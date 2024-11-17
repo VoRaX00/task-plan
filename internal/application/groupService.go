e12adcfcea407c8c8e28a61ee6c0a7738c49bd44
nikita <nikitakerzhik@gmail.com>
2024-11-17 14:07:35 +1000

    edit service

--- internal/application/groupService.go
+++ internal/application/groupService.go
@@ -1,9 +1,19 @@
 package application
 
+import "task-plan/internal/application/requestModels"
+
 type GroupService struct {
 	repo *ITaskService
 }
 
 func NewGroupService(repo *ITaskService) *GroupService {
 	return &GroupService{repo: repo}
+}
+
+func (s *GroupService) Create(add requestModels.GroupToAdd) (int, error) {
+	return 0, nil
+}
+
+func (s *GroupService) GetById(id int) (requestModels.GroupToGet, error) {
+	return requestModels.GroupToGet{}, nil
 }
--- internal/application/taskService.go
+++ internal/application/taskService.go
@@ -1,9 +1,19 @@
 package application
 
+import "task-plan/internal/application/requestModels"
+
 type TaskService struct {
 	repo *ITaskRepository
 }
 
 func NewTaskService(repo *ITaskRepository) *TaskService {
 	return &TaskService{repo: repo}
+}
+
+func (s *TaskService) Create(task requestModels.TaskToAdd) (int, error) {
+	return 0, nil
+}
+
+func (s *TaskService) GetById(id int) (requestModels.TaskToGet, error) {
+	return requestModels.TaskToGet{}, nil
 }
